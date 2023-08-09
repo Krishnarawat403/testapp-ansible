6dfb61a927ae2fc3ebc9c8b19a5e3cd5ca698af9
Krishnarawat403 <98050008+Krishnarawat403@users.noreply.github.com>
2023-08-09 15:55:14 -0400

    Create index.js

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Dockerized React Frontend!</h1>
+      {/* Add your other components and content here */}
+    </div>
+  );
+}
+
+export default App;
